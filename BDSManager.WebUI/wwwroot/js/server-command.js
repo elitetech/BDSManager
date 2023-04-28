0d22b19e584265d81c1aba042183526def51036f
Koty Bashford <koty.bashford@gmail.com>
2023-04-27 22:58:12 -0400

    Added Time command

--- BDSManager.WebUI/wwwroot/js/server-command.js
+++ BDSManager.WebUI/wwwroot/js/server-command.js
@@ -11,6 +11,18 @@ function giveCommand(button){
     let player = $('button[aria-labelledby="server-command-give-item"][aria-expanded="true"]').attr('data-player-name');
     let item = $('button[aria-labelledby="server-command-give-amount"][aria-expanded="true"]').attr('data-item-id-name');
     let amount = $(button).attr('data-item-amount');
+    if (player == undefined && item == undefined){
+        alertToast('Error: Player and Item not defined!')
+        return;
+    }
+    if (player == undefined){
+        alertToast('Error: Player not defined!');
+        return;
+    }
+    if (item == undefined){
+        alertToast('Error: Item not defined!');
+        return;
+    }
     $(`input#server-command-${path}`).val(`give ${player} ${item} ${amount}`);
     $('body').click();
     $(`input#server-command-${path}`).focus();
@@ -43,8 +55,63 @@ function tpCommand(button){
         let z = $(button).parent().find('input.server-command-tp-player-z').val();
         target = `${x} ${y} ${z}`;
     }
-
+    if (player == undefined && target == undefined){
+        alertToast('Error: Player and Target not defined!')
+        return;
+    }
+    if (player == undefined){
+        alertToast('Error: Player not defined!');
+        return;
+    }
+    if (target == undefined){
+        alertToast('Error: Target not defined!');
+        return;
+    }
     $(`input#server-command-${path}`).val(`tp ${player} ${target}`);
+    $('body').click();
+    $(`input#server-command-${path}`).focus();
+}
+
+function timeSetCommand(button){
+    let path = $(button).attr('data-server-path');
+    let time = $(button).attr('data-time');
+
+    if (time == undefined){
+        time = $(button).parent().find('input.server-command-time-set').val();
+    }
+
+    if (time == undefined){
+        alertToast('Error: Ticks/time not defined!');
+        return;
+    }
+    $(`input#server-command-${path}`).val(`time set ${time}`);
+    $('body').click();
+    $(`input#server-command-${path}`).focus();
+}
+
+function timeAddCommand(button){
+    let path = $(button).attr('data-server-path');
+    let time = $(button).parent().find('input.server-command-time-add').val();
+    
+
+    if (time == undefined){
+        alertToast('Error: Ticks not defined!');
+        return;
+    }
+    $(`input#server-command-${path}`).val(`time add ${time}`);
+    $('body').click();
+    $(`input#server-command-${path}`).focus();
+}
+
+function timeQueryCommand(button){
+    let path = $(button).attr('data-server-path');
+    let time = $(button).attr('data-query');
+
+    if (time == undefined){
+        alertToast('Error: Query not defined!');
+        return;
+    }
+    $(`input#server-command-${path}`).val(`time query ${time}`);
     $('body').click();
     $(`input#server-command-${path}`).focus();
 }
